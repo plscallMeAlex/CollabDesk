17034e4e7bd9c5d5d9fbc771f8e1a8be855ac1a5
plscallMeAlex <alexkung007@gmail.com>
2025-02-17 17:31:20 +0700

    fix: input

--- server/start.sh
+++ server/start.sh
@@ -1,4 +1,4 @@
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 python manage.py runserver 0.0.0.0:8000
