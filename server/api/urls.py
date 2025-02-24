0b4dfa8780663228771a2a65732945ee28c6a00e
plscallMeAlex <alexkung007@gmail.com>
2025-02-25 01:36:16 +0700

    urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.views.task_view import TaskViewSet
+from api.views.taskstate_view import TaskStateViewSet
 from api.views.role_view import RoleViewSet
-from api.views.view import home
 from api.views.user_view import UserViewSet
+from api.views.view import home
 
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="users")  # ip/users
 router.register(r"tasks", TaskViewSet, basename="tasks")  # ip/tasks
 router.register(r"roles", RoleViewSet, basename="roles")  # ip/roles
-
+router.register(r"taskstates", TaskStateViewSet, basename="taskstates")  # ip/taskstates
 
 urlpatterns = [
     path("", home, name="home"),
