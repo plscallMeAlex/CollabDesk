3a9bbb93a5c85699bd113850596aa2e61de4209c
plscallMeAlex <alexkung007@gmail.com>
2025-02-25 01:25:02 +0700

    taskstate

---
+++ server/api/models/taskstate.py
@@ -0,0 +1,8 @@
+from django.db import models
+import uuid
+
+
+class TaskState(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=20)
+    guild = models.ForeignKey("Guild", on_delete=models.CASCADE)
