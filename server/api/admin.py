b1bfbeaf52c369918593bbf808a2a24de2e9fbf3
plscallMeAlex <alexkung007@gmail.com>
2025-02-27 14:12:01 +0700

    add admin router

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import TaskState, Task, User, Guild
+
+admin.site.register(TaskState)
+admin.site.register(Task)
+admin.site.register(User)
+admin.site.register(Guild)
