3a9781396df00dd3e3d276d520f00633c4f9583d
plscallMeAlex <alexkung007@gmail.com>
2025-02-28 16:27:00 +0700

    add/add user data inside the config

--- app/Configuration.py
+++ app/Configuration.py
@@ -10,6 +10,7 @@ class Configuration:
         self.colors = self.__color_import()
         self.font = "Inter"
         self.api_url = os.getenv("API_URL")
+        self.user_data = {"id": "f452d1d8-f836-4006-a63d-647e03836040"}
         self.__setup()
 
     def __setup(self):
