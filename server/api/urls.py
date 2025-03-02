5c10edaca367e1c65e0d9ce903bf4b171ff332dc
plscallMeAlex <alexkung007@gmail.com>
2025-03-02 11:49:29 +0700

    add/urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from api.views.task_view import TaskViewSet
 from api.views.taskstate_view import TaskStateViewSet
 from api.views.role_view import RoleViewSet
@@ -16,4 +17,6 @@ router.register(r"taskstates", TaskStateViewSet, basename="taskstates")  # ip/ta
 urlpatterns = [
     path("", home, name="home"),
     path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
