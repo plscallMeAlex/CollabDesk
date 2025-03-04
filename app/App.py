4ba7296378b18ff420c5c94cc9376879b6f5e413
plscallMeAlex <alexkung007@gmail.com>
2025-03-04 18:05:38 +0700

    fix/naming

--- app/App.py
+++ app/App.py
@@ -18,10 +18,10 @@ class App(ctk.CTk):
         self.minsize(1920, 1080)
 
         self.title("CollabDesk")
-        self.config = Configuration()
+        self.configuration = Configuration()
 
         # Set main background color
-        self.configure(fg_color=self.config.colors["frame-color-secondary"])
+        self.configure(fg_color=self.configuration.colors["frame-color-secondary"])
 
         # Pagemanagement System
         self.pagemanager = Pagemanager(self)
@@ -31,19 +31,19 @@ class App(ctk.CTk):
         self.check_user_token()
 
         # Add Header to the top of the app window but make it compact
-        self.header = Header(self)  # Create an instance of Header
-        self.header.pack(
-            side="top", fill="x", pady=10
-        )  # Add padding to make it compact
+        # self.header = Header(self)  # Create an instance of Header
+        # self.header.pack(
+        # side="top", fill="x", pady=10
+        # )  # Add padding to make it compact
 
         # Add Sidebar to the main app
         # Add Sidebar to the main app (Left Side)
-        self.sidebar_frame = SidebarFrame(self)
-        self.sidebar_frame.pack(side="left", fill="y", padx=10, pady=10)
+        # self.sidebar_frame = SidebarFrame(self)
+        # self.sidebar_frame.pack(side="left", fill="y", padx=10, pady=10)
 
         # Add ChannelBar next to Sidebar (Left Side, but after Sidebar)
-        self.ChannelBar = ChannelBar(self)
-        self.ChannelBar.pack(side="left", fill="y", padx=10, pady=10)
+        # self.ChannelBar = ChannelBar(self)
+        # self.ChannelBar.pack(side="left", fill="y", padx=10, pady=10)
 
         if sys.platform.startswith("win"):
             self.after(100, self.__maximize)
