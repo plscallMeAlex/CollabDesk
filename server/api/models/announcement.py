c5e61bf3c354d207e6a08afe007cef341218a28f
plscallMeAlex <alexkung007@gmail.com>
2025-03-29 20:34:16 +0700

    announcement field

--- server/api/models/announcement.py
+++ server/api/models/announcement.py
@@ -1,10 +1,12 @@
 from django.db import models
-from api.models import Guild
+from api.models import Guild, User
+
 
 class Announcement(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     guild = models.ForeignKey(Guild, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- server/api/views/announcement_view.py
+++ server/api/views/announcement_view.py
@@ -11,15 +11,18 @@ class AnnouncementViewSet(ModelViewSet):
     queryset = Announcement.objects.all()
     serializer_class = AnnouncementSerializer
 
-    @action(detail=False, methods=['get'])
+    @action(detail=False, methods=["get"])
     def get_announcements_by_guild(self, request):
-        guild_id = request.query_params.get('guild_id')
+        guild_id = request.query_params.get("guild_id")
         if guild_id is None:
             return Response(status=status.HTTP_400_BAD_REQUEST)
         guild = Guild.objects.filter(id=guild_id).first()
         if guild is None:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        
+
         announcements = Announcement.objects.filter(guild=guild)
+        if not announcements.exists():
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
         serializer = AnnouncementSerializer(announcements, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
