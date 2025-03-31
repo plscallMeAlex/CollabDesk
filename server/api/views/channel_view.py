c6693896ab4828fa3f89458abe5a1a17f6a70315
plscallMeAlex <alexkung007@gmail.com>
2025-03-31 23:34:38 +0700

    crud message

--- server/api/views/channel_view.py
+++ server/api/views/channel_view.py
@@ -10,20 +10,6 @@ class ChannelViewSet(ModelViewSet):
     queryset = Channel.objects.all()
     serializer_class = ChannelSerializer
 
-    @action(detail=False, methods=["GET"])
-    def get_messages(self, request):
-        channel_id = request.query_params.get("channel_id")
-        if channel_id is None:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        channel = Channel.objects.filter(id=channel_id).first()
-        if channel is None:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        if channel.type != "text":
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        messages = Message.objects.filter(channel=channel).order_by("created_at")
-        serializer = ChannelSerializer(messages, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
     @action(detail=False, methods=["POST"])
     def create_channel(self, request):
         serializer = self.get_serializer(data=request.data)
