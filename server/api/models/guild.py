4955e17519a717409bf6cb7c8615995f7d599b14
plscallMeAlex <alexkung007@gmail.com>
2025-04-02 02:05:03 +0700

    create invitetoken

--- server/api/models/guild.py
+++ server/api/models/guild.py
@@ -1,29 +1,17 @@
 from django.db import models
 import uuid
-import random
-import string
-
-
-def generate_invite_token():
-    """Generate a unique 8-character invite token."""
-    return "".join(random.choices(string.ascii_letters + string.digits, k=8))
 
 
 class Guild(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50)
     invitetoken = models.CharField(
-        max_length=8, unique=True, default=generate_invite_token, editable=False
+        max_length=8, unique=True, blank=True, editable=False, null=True, default=None
     )
 
     def save(self, *args, **kwargs):
-        # Ensure the invite token is unique
-        if not self.invitetoken:
-            while True:
-                token = generate_invite_token()
-                if not Guild.objects.filter(invitetoken=token).exists():
-                    self.invitetoken = token
-                    break
+        if not self.invitetoken:  # Only generate if empty
+            self.invitetoken = uuid.uuid4().hex[:8]
         super().save(*args, **kwargs)
 
     def __str__(self):
