63c6b5efca9b3da8a107caff8589b189530c2f09
plscallMeAlex <alexkung007@gmail.com>
2025-04-02 17:42:04 +0700

    handle task id+

--- server/api/tasksmail.py
+++ server/api/tasksmail.py
@@ -1,11 +1,11 @@
+# api/tasks.py
 from celery import shared_task
 from django.core.mail import send_mail
 from dateutil import parser
 
 
 @shared_task
-def send_email_task(subject, message, from_email, recipient_list):
-    """Task to send an email using Celery."""
+def send_scheduled_email(subject, message, from_email, recipient_list, task_id=None):
     send_mail(
         subject=subject,
         message=message,
@@ -15,11 +15,45 @@ def send_email_task(subject, message, from_email, recipient_list):
     )
 
 
-def schedule_email(subject, message, from_email, recipient_list, send_time):
-    """Schedule an email to be sent at a specific time."""
-    utc_datetime = parser.parse(send_time)
+def schedule_email(
+    subject, message, from_email, recipient_list, scheduled_time_str, task_id
+):
+    utc_datetime = parser.parse(scheduled_time_str)
 
-    send_email_task.apply_async(
+    # Include a task_id in kwargs that can be used for identification
+    task = send_scheduled_email.apply_async(
         args=[subject, message, from_email, recipient_list],
-        eta=utc_datetime,  # Set the execution time to the parsed UTC datetime
+        eta=utc_datetime,
+        kwargs={"task_id": task_id},
     )
+
+    # Return the task ID so it can be stored or used for updates
+    return task.id
+
+
+def update_scheduled_email(
+    task_id,
+    subject=None,
+    message=None,
+    from_email=None,
+    recipient_list=None,
+    new_scheduled_time_str=None,
+):
+    from celery.app.control import revoke
+
+    # Revoke the existing task
+    revoke(task_id, terminate=True)
+
+    # Schedule a new task with updated information
+    utc_datetime = (
+        parser.parse(new_scheduled_time_str) if new_scheduled_time_str else None
+    )
+
+    # Use the provided task_id directly instead of generating a new one
+    new_task = send_scheduled_email.apply_async(
+        args=[subject, message, from_email, recipient_list],
+        eta=utc_datetime,
+        kwargs={"task_id": task_id},  # Use the task_id parameter directly
+    )
+
+    return new_task.id
